4e9afd107c54a67f424a7de084cfa972b47bcd56
Vinicius Davi <viniprogrammer@hotmail.com>
2023-02-09 12:56:32 -0300

    new uodate

--- Aulas_de_C/Aula137b.c
+++ Aulas_de_C/Aula137b.c
@@ -8,10 +8,16 @@ int main(){
     char palavras1[100] = {"manchester united"}, palavras2[100] = {"santos"};
     int i = 0;
 
+    printf("Strings originais\n");
+
+    printf("%s e %s\n", palavras1, palavras2);
+
     while(palavras1[i] != '\0'){
         palavras1[i] = palavras2[i];
         i++;
     }
+
+    printf("\nStrings modificadas\n");
 
     printf("%s e %s\n", palavras1, palavras2);
 
